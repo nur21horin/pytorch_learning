dc51d7934bf3afa187f16e16e0e64bad3fa9d7d5
Nur Mohammod <nurm98371@gmail.com>
2026-05-23 11:44:16 +0600

    move tensor operations to a new file and clean up main.py

--- main.py
+++ main.py
@@ -40,24 +40,3 @@ random_tensor=torch.rand(3,3)
 
 print(random_tensor)
 
-#Tensor operation
-x1=torch.tensor([1,2,3])
-x2=torch.tensor([4,5,6])
-#add
-print(x1+x2)
-# print(torch.add(x1,x2))
-# #subtract
-# print(x1-x2)
-# print(torch.subtract(x1,x2))
-# #multiply
-# print(x1*x2)
-# print(torch.multiply(x1,x2))
-# #divide
-# print(x1/x2)
-# print(torch.divide(x1,x2))
-# #dot product
-# print(torch.dot(x1,x2))
-
-# #matrix multiplication
-# x1=torch.tensor([[1,2],[3,4]])
-# x2=torch.tensor([[5,6],[7,8]])  
---
+++ tensor_operation.py
@@ -0,0 +1,31 @@
+import torch
+#Tensor operation
+x1=torch.tensor([1,2,3])
+x2=torch.tensor([4,5,6])
+
+#add
+print(x1+x2)
+print(torch.add(x1,x2))
+
+ #subtract
+print(x1-x2)
+print(torch.subtract(x1,x2))
+
+ #multiply
+print(x1*x2)
+print(torch.multiply(x1,x2))
+
+#divide
+print(x1/x2)
+print(torch.divide(x1,x2))
+#dot product
+print(torch.dot(x1,x2))
+
+#matrix multiplication
+x1=torch.tensor([[1,2],[3,4]])
+x2=torch.tensor([[5,6],[7,8]])  
+
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+#print(torch.cuda.current_device())
+#print(torch.cuda.get_device_name(0))
